99c2d4a94426ac5a756637bf7eac580ccc4f22d1
crispindeity <shoy1415@gmail.com>
2022-12-16 12:36:48 +0900

    의존성 추가

--- src/main/java/study/springstandard/ch2/Hello.java
+++ src/main/java/study/springstandard/ch2/Hello.java
@@ -2,13 +2,14 @@ package study.springstandard.ch2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Hello {
     int i = 10;
     static int si = 30;
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     private void main() {
         System.out.println("Hello");
         System.out.println("i = " + i);
