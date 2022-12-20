bbd6cddb2c321836bd31a304fcac30b28571c485
crispindeity <shoy1415@gmail.com>
2022-12-21 05:01:00 +0900

    :sparkles: Issue Service
    Discreption: 새로운 이슈 서비스 로직 구현

---
+++ src/main/java/study/springstandard/ch2/MethodInfo.java
@@ -0,0 +1,16 @@
+package study.springstandard.ch2;
+
+public class MethodInfo {
+    public static void main(String[] args) throws Exception {
+        Class<?> yoilTellerClass = Class.forName("study.springstandard.ch2.YoilTeller");
+        Object obj = yoilTellerClass.newInstance();
+        MethodInfo methodInfo = new MethodInfo();
+        methodInfo.oldOne();
+
+    }
+
+    @Deprecated(since="9")
+    public void oldOne() {
+
+    }
+}
